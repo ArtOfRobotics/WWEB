058780d33848e257c268ffece7bc75160bf1f96a
lalilaloe <jonathantenhove@hotmail.com>
2018-03-27 14:31:06 +0200

    init

---
+++ listener.js
@@ -0,0 +1,20 @@
+// Require rosnodejs itself
+const rosnodejs = require('rosnodejs');
+
+// Requires the std_msgs message package
+const std_msgs = rosnodejs.require('std_msgs').msg;
+
+function listener() {
+  // Register node with ROS master
+  rosnodejs.initNode('/listener_node')
+    .then((rosNode) => {
+      // Create ROS subscriber on the 'chatter' topic expecting String messages
+      let sub = rosNode.subscribe('/sonar', std_msgs.String,
+        (data) => { // define callback execution
+          rosnodejs.log.info('I heard: [' + data.data + ']');
+        }
+      );
+    });
+}
+
+listener();
